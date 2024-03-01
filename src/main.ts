4fce508a072312fcc8889678073e6a3e4c4de5ce
Philip Smolen <philip@trade-ideas.com>
2024-02-29 22:38:11 -0800

    Minor changes to formatting.
    Lots of minor cleanup
    and code reorganization.

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,41 @@
-import { initializedArray } from 'phil-lib/misc';
-import './style.css';
-import { getById } from 'phil-lib/client-misc';
+import { initializedArray } from "phil-lib/misc";
+import "./style.css";
+import { getById } from "phil-lib/client-misc";
 
-const SIZE = 8
+const SIZE = 8;
 
-function findGrid(id:string){
+function findGrid(id: string) {
   const top = getById(id, HTMLDivElement);
   top.classList.add("grid");
-  const byRow = initializedArray(SIZE, () =>  initializedArray(SIZE, () => {
-    const cell = document.createElement("div");
-    top.appendChild(cell);
-    return cell;
-  }) );
-  return {top, byRow};
+  const byRow = initializedArray(SIZE, () =>
+    initializedArray(SIZE, () => {
+      const cell = document.createElement("div");
+      top.appendChild(cell);
+      return cell;
+    })
+  );
+  const all = byRow.flat();
+  return { top, byRow, all };
 }
 
 const inputGrid = findGrid("input");
 
 const period = 5000;
-inputGrid.byRow.forEach((row) => row.forEach(cell =>{
-  cell.animate([{offset:0, backgroundColor:"rgba(0,0,0,0)"},{offset:0.05, backgroundColor:"rgba(0,0,0,.1)"},{offset:0.1, backgroundColor:"rgba(0,0,0,0)"},], {duration:period, iterations:Infinity, delay:Math.random()*period });
-}))
+inputGrid.all.forEach((cell) => {
+  cell.animate(
+    [
+      { offset: 0, backgroundColor: "rgba(0,0,0,0)" },
+      { offset: 0.05, backgroundColor: "rgba(0,0,0,.1)" },
+      { offset: 0.1, backgroundColor: "rgba(0,0,0,0)" },
+    ],
+    { duration: period, iterations: Infinity, delay: Math.random() * period }
+  );
+});
 
-const all = inputGrid.byRow.flat();
+const all = inputGrid.all;
 let index = 0;
-all[index++].innerText="0";
-all[index++].innerText="0.00";
-all[index++].innerText=".00";
-all[index++].innerText=".987654";
-all[index++].innerHTML="5<br>5";
+all[index++].innerText = "0";
+all[index++].innerText = "0.00";
+all[index++].innerText = ".00";
+all[index++].innerText = ".987654";
+all[index++].innerHTML = "5<br>5";
